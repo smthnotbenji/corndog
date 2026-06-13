3245082aa7328bd3475e950b7390b2227510a880
smthnotbenji <smthnotbenji@gmail.com>
2026-06-13 17:21:23 -0400

    Create meow.js

---
+++ commands/meow.js
@@ -0,0 +1,12 @@
+export default {
+  name: "meow",
+  description: ":3"
+  async execute(interaction, env) {
+    return {
+      type: 4,
+      data: {
+        content: `Meow`
+      }
+    };
+  }
+};
