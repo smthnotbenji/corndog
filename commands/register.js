7541d9eb625bdec2b55a686b9c0459a85476a008
smthnotbenji <smthnotbenji@gmail.com>
2026-06-13 18:47:15 -0400

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,9 +1,11 @@
 import echo from "./echo.js";
 import meow from "./meow.js";
+import note from "./note.js";
 
 const commands = new Map([
   [echo.name, echo],
-  [meow.name, meow]
+  [meow.name, meow],
+  [note.name, note]
 ]);
 
 export function getCommand(name) {
