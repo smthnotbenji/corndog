5d720d3d289cc0af6304108ce9fbc7842fabc0c3
smthnotbenji <smthnotbenji@gmail.com>
2026-06-13 17:07:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ export default {
     try {
       console.log("REQUEST RECEIVED");
 
+      const url = new URL(request.url);
+
+if (url.pathname === "/register") {
+const commands = [
+  {
+    name: "echo",
+    description: "Make corndog say a thing",
+        options: [
+      {
+        name: "text",
+        description: "The text",
+        type: 3,
+        required: true
+      }
+    ]
+  }
+];
+  
+
+  const res = await fetch(
+    `https://discord.com/api/v10/applications/${env.APP_ID}/guilds/${env.GUILD_ID}/commands`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bot ${env.TOKEN}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(commands)
+    }
+  );
+
+  return new Response(await res.text());
+}
       if (request.method !== "POST") {
         return new Response("Not allowed", { status: 405 });
       }
